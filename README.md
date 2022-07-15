6b8ed15cb8c507d8c4df5e48be1fe86781c03096
rita2021.zhang@gmail.com <rita2021.zhang@gmail.com>
2022-07-15 13:25:52 -0500

    added gridOptions

--- README.md
+++ README.md
@@ -25,3 +25,28 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+## AG Grid Interface
+
+https://www.ag-grid.com/angular-data-grid/grid-interface/
+
+###The grid interface is the combination of the following items:
+。Grid Options: properties and callbacks used to configure the grid, e.g. pagination = true and getRowHeight(params).
+
+。Grid API: methods used to interact with the grid after it's created, e.g. api.getSelectedRows().
+
+。Grid Events: events published by the grid to inform applications of changes in state, e.g. rowSelected.
+
+。Row Node: each row in the grid is represented by a Row Node object, which in turn has a reference to the piece of row data provided by the application. The Row Node wraps the row data item. The Row Node has attributes, methods and events for interacting with the specific row e.g. rowNode.setSelected(true).
+
+###Properties, Events, Callbacks and APIs
+。Properties: properties are bound via @Inputs (e.g. [columnDefs]="columnDefs").
+
+。Attributes: attributes are properties that also accept literal values (e.g. rowSelection="multiple").
+
+。Callbacks: callbacks are bound via @Inputs like properties (e.g. [getRowHeight]="myGetRowHeight").
+
+。Event Handlers: event handlers are are bound via @Outputs (e.g. (cellClicked)="onCellClicked($event)").
+
+。API: the grid API and column API are accessible through the component.
+
--- src/app/agGrid/ag-grid-demo/ag-grid-demo.component.ts
+++ src/app/agGrid/ag-grid-demo/ag-grid-demo.component.ts
@@ -1,5 +1,5 @@
 import {Component, ViewChild} from '@angular/core';
-import {CellClickedEvent, ColDef, GridReadyEvent} from "ag-grid-community";
+import {CellClickedEvent, ColDef, GridOptions, GridReadyEvent} from "ag-grid-community";
 import {from, Observable, of} from "rxjs";
 import {AgGridAngular} from "ag-grid-angular";
 import {HttpClient} from "@angular/common/http";
@@ -25,17 +25,38 @@ export class AgGridDemoComponent  {
     filter: true,
   };
 
+  public gridOptions : GridOptions = {
+    // PROPERTIES
+    // Objects like myRowData and myColDefs would be created in your application
+    rowData: rowData2,
+    columnDefs: this.columnDefs,
+    pagination: true,
+    rowSelection: 'single',
+
+    // EVENTS
+    // Add event handlers
+    onRowClicked: event => console.log('A row was clicked'),
+    onColumnResized: event => console.log('A column was resized'),
+    onGridReady: event => console.log('The grid is now ready'),
+
+    // CALLBACKS
+    getRowHeight: (params) => 25
+  }
+
+
   // Data that gets displayed in the grid
   public rowData$!: Observable<any[]>;
   public rowData2!: Observable<any[]>;
 
   // For accessing the Grid's API
+  // you can retrieve the grid component with a @ViewChild attribute from within your component.
   @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
 
   constructor(private http: HttpClient) {}
 
   // Example load data from sever
   // $ 是一个约定，表示 rowData$ 是一个 Observable 而不是数组
+  // When the grid is initialised, it will fire the gridReady event.
    onGridReady(params: GridReadyEvent) {
   //   this.rowData$ = this.http
   //     .get<any[]>('https://www.ag-grid.com/example-assets/row-data.json');
@@ -46,12 +67,17 @@ export class AgGridDemoComponent  {
   // Example of consuming Grid Event
   onCellClicked( e: CellClickedEvent): void {
     console.log('cellClicked', e);
+
   }
 
   // Example using Grid's API
   clearSelection(): void {
     console.log("agGrid", this.agGrid);
     this.agGrid.api.deselectAll();
+    // refresh the grid
+    this.gridOptions.api?.redrawRows();
+    // resize columns in the grid to fit the available space
+    this.gridOptions.columnApi ?.sizeColumnsToFit(0);
   }
 
 }
